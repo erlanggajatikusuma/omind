efb3aef24474d985fd451c2f7b204a7ada15f796
kusuma <erlanggajatikusuma007@gmail.com>
2021-01-19 15:19:11 +0700

    2nd commit

--- src/containers/pages/index.js
+++ src/containers/pages/index.js
@@ -1,20 +1,68 @@
-import React, {useState} from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {StyleSheet, Text, View, TouchableOpacity} from 'react-native';
 import {Picker} from '@react-native-picker/picker';
 import Input from '../../components/atoms/Input';
 import Buttons from '../../components/atoms/Button';
+import {launchCamera, launchImageLibrary} from 'react-native-image-picker';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 const DiscussPage = () => {
   const [state, setState] = useState({
+    image: '',
+    groupName: '',
     jenjang: '',
   });
 
+  const takeImage = () => {
+    launchCamera(
+      {
+        mediaType: 'photo',
+        includeBase64: true,
+        maxHeight: 700,
+        maxWidth: 700,
+        quality: 1,
+      },
+      (response) => {
+        console.log('response', response);
+        if (response.didCancel || response.error) {
+          console.log('gagal');
+        } else {
+          console.log('Photo: ', response);
+          //   const source = {uri: response.uri};
+          //   const base64Photo = `data:${response.type};base64, ${response.data}`;
+        }
+      },
+    );
+  };
+
+  useEffect(() => {
+    fetch('https://www.postman.com/collections/76c330db5471fe4afda4')
+      .then((response) => response.json())
+      .then((json) => console.log(json.item));
+    return () => {
+      null;
+    };
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Input styles={styles.input} placeholder="Nama Groups" />
+      <View
+        style={{
+          width: 100,
+          height: 100,
+          backgroundColor: 'grey',
+          justifyContent: 'center',
+          alignItems: 'center',
+          alignSelf: 'center',
+        }}>
+        <TouchableOpacity onPress={takeImage}>
+          <Icon name="camera" size={25} color="black" />
+        </TouchableOpacity>
+      </View>
+      <Input styles={styles.input} placeholder="Nama Group" />
       <Picker
         selectedValue={state.jenjang}
-        style={{height: 50, width: 100}}
+        style={{borderRadius: 10, backgroundColor: '#ddd'}}
         onValueChange={(itemValue, itemIndex) =>
           setState({jenjang: itemValue})
         }>
@@ -37,15 +85,21 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
+    paddingHorizontal: 35,
+    backgroundColor: '#fff',
   },
   input: {
     borderWidth: 1,
-    backgroundColor: '#fff',
+    borderRadius: 10,
+    backgroundColor: '#ddd',
+    color: 'black',
   },
   btn: {
-    backgroundColor: '#eee',
+    backgroundColor: '#4956fc',
     paddingVertical: 10,
-    marginHorizontal: 20,
     textAlign: 'center',
+    color: '#eee',
+    fontWeight: 'bold',
+    borderRadius: 10,
   },
 });
